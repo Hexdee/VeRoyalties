84c14bcc3a21890bb6e9a9129f436fcf933e5cc9
Hexdee <lukmansaheed79@gmail.com>
2022-07-13 18:43:05 +0100

    Remove fake property from marketplace

--- frontend/src/components/pages/MarketPlace.jsx
+++ frontend/src/components/pages/MarketPlace.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./About.css";
-import Property from '../misc/Property';
-import properties from "../../datas/properties"
+// import Property from '../misc/Property';
+// import properties from "../../datas/properties"
+import comingSoon from "../../images/coming-soon-p.png"
 import { useEffect } from 'react';
 
 const MarketPlace = () => {
@@ -14,13 +15,14 @@ const MarketPlace = () => {
             <section className="about">
                 <h1 className='page-heading'>MarketPlace</h1>
                 <div className="market-contents">
-                  <div className="pr-header">
+                <div className='coming-soon' style={{ backgroundImage: `url(${comingSoon})` }}></div>
+                  {/* <div className="pr-header">
                     <h3 id="properties">Among our properties already financed</h3>
                     <h3 className="cl-blue">View All</h3>
                   </div>
                   <div className="properties">
                   {properties.map((property) => <Property property={property}/>)}
-                  </div>
+                  </div> */}
                 </div>
             </section>
         </React.Fragment>
